6e3b77af5623c17d8ffb8d08df22a8fba197be31
tripy37 <kevin.gray@amdtel.co.uk>
2023-07-28 12:04:11 +0100

    Configure supabase url and public key

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bankljgliwcehnurvnrn.supabase.co";
+const supabaseUrl = "https://soteogxtiwrjddjykvsy.supabase.co";
 const supabaseAnonPublic =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhbmtsamdsaXdjZWhudXJ2bnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE4MDQ5NDMsImV4cCI6MTk4NzM4MDk0M30.EuXR6hGhMkCNvgNs_Th8S-9M558IXPQfehGsvQIazoE";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvdGVvZ3h0aXdyamRkanlrdnN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAzMDk0NjgsImV4cCI6MjAwNTg4NTQ2OH0.8zM75ID7GeBavpadvdA-86l0IbpSw1E_YuO3c8F_2D8";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonPublic);
